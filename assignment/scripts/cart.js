15a33e5ff31bd69ba48d13ba0cb5042627cef180
Paul Norling <paulnorling87@gmail.com>
2022-10-13 09:24:48 -0500

    thurs 9:24

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,13 +3,12 @@ console.log('***** Cart Functions *****');
 let basket = [];
 const maxItems = 5;
 
-console.log('Basket is full:',isFull());
+console.log('Basket is full?:',isFull());
 
 function addItem(item){
   if (isFull()==false){basket.push(item)
     console.log('Added item:',item,true);
-  }
-  else {
+  }else {
     console.log('Added item:',item,false);
   }
 };
@@ -17,8 +16,7 @@ function addItem(item){
 function isFull(){
   if(basket.length<maxItems){
     return false;
-  }
-  else{
+  }else {
     return true;
   }
 };
@@ -30,38 +28,47 @@ addItem('consumables');
 addItem('battery');
 addItem('pump');
 
-console.log('Basket is full:',isFull());
-
 function empty(){
   basket.length=0;
 }
 
+
+
+console.log('Basket is full?:',isFull());
+
 function listItems(){
-  for (item of basket){
+  {console.log('BASKET:')
+  }for (let item of basket){
     console.log(item)
   }
-}
+};
+
 listItems();
 
 function removeItem(item){
-  let match = false;{
-  for (let i=0; i<basket.length; i++)
+  let match = false;
+  for (let i=0; i<basket.length; i++){
     if (basket[i]==item){
-      match = true;
-    }
-  }
-  if(match){
+      match = true;}
+  }if(match){
     console.log('Items removed:',item),
     basket.splice(basket.indexOf(item),1)
   }else {
     console.log('Items removed: null')
-  } 
-;}
+  }
+};
        
 
 removeItem('bushings');
 removeItem('adapter');
 removeItem([1,2,3,4]);
-console.log(basket);
+listItems();
+console.log('Basket is full?:',isFull());
+addItem('pulley');
+empty();
+listItems();
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+/*let basketFull = true;
+let detergentEmpty = false;
+*/
