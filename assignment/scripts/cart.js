939cd320be7661d6b8b364f418beb955970a3cfb
Paul Norling <paulnorling87@gmail.com>
2022-10-16 18:57:11 -0500

    rearranged testing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,9 +1,8 @@
 console.log('***** Cart Functions *****');
 
 let basket = [];
-const maxItems = 5;
 
-console.log('Basket is full?:',isFull());
+const maxItems = 5;
 
 function addItem(item){
   if (isFull()==false){basket.push(item)
@@ -11,11 +10,31 @@ function addItem(item){
   }else {
     return false;
   }
-};
+}
 
 function isFull(){
-  return (basket.length < maxItems) ? false : true;
-};
+  return (basket.length >= maxItems);
+}
+
+function empty(){
+  basket.length=0;
+}
+
+function listItems(){
+  for (let item of basket){
+    console.log(item);
+  }
+}
+
+function removeItem(item){
+  if(basket.includes(item)){
+    basket.splice(basket.indexOf(item),1)
+    return item;
+  }else {
+    return 'null';
+  }
+}
+
 
 console.log('added: bearings', addItem('bearings'));
 console.log('added: axle', addItem('axle'));
@@ -23,41 +42,20 @@ console.log('added: bushings', addItem('bushings'));
 console.log('added: consumables', addItem('consumables'));
 console.log('added: battery',addItem('battery'));
 console.log('added: pump', addItem('pump'));
-console.log('added: ldskfv', addItem("[]"));
-
-function empty(){
-  basket.length=0;
-}
-
-console.log('Basket is full?:',isFull());
-
-function listItems(){
-  for (let item of basket){
-    console.log(item)
-  }
-};
-
+console.log('added: 5', addItem());
 listItems();
-
-
-function removeItem(item){
-  if(basket.includes(item)){
-    basket.splice(basket.indexOf(item),1)
-    return('Items removed:' + ' ' + item)
-  }else {
-    return('Items removed: null')
-  }
-};
-
-console.log(removeItem('bushings'));
-console.log(removeItem('adapter'));
-console.log(removeItem([1,2,3,4]));
-console.log(removeItem('string'));
+console.log('removed', removeItem('bushings'));
+console.log('removed', removeItem('adapter'));
+console.log('removed', removeItem([1,2,3,4]));
+console.log('removed',removeItem(5));
 listItems();
 console.log('Basket is full?:',isFull());
 console.log('added pulley',addItem('pulley'));
 empty();
 listItems();
+
+
+
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
